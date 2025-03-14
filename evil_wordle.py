cc34d50c49e0aa13c0875fdc6a2f344115366a11
markFak <103273896+meekymerkmeek@users.noreply.github.com>
2025-03-13 23:49:10 -0500

    Update evil_wordle.py

--- evil_wordle.py
+++ evil_wordle.py
@@ -123,7 +123,7 @@ class Keyboard:
         formatted_rows = []
 
         for i, row in enumerate(self.rows):
-            leading_spaces = " " * (i * 2)  
+            leading_spaces = " " * i  
             formatted_row = leading_spaces + " ".join(color_word(self.colors[letter], letter) for letter in row)
             formatted_rows.append(formatted_row)
 
